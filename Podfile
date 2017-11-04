4854ed84fdd63407f1fc70b76e03f17395f8cd92
Ryan Yoo <yoobh17@gmail.com>
2017-11-04 20:49:58 +0900

    init

---
+++ Podfile
@@ -0,0 +1,11 @@
+
+platform :ios, '10.0'
+
+target 'Yum' do
+  use_frameworks!
+
+  pod 'SwiftLint'
+  
+  pod 'Alamofire'
+  pod 'AlamofireImage'
+end
---
+++ Yum/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  Yum
+//
+//  Created by Ryan Yoo on 2017. 11. 4..
+//  Copyright © 2017년 Mash-up. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+    
+}
